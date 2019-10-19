6f515ea822eb589937c881cea32d53ac34330422
azoraw <al.zoraw@gmail.com>
2019-10-19 16:35:36 +0200

    Prettier output string for  ScreeningNotFoundException

--- src/main/java/com/zoraw/cinema/model/exception/ScreeningNotFoundException.java
+++ src/main/java/com/zoraw/cinema/model/exception/ScreeningNotFoundException.java
@@ -2,11 +2,7 @@ package com.zoraw.cinema.model.exception;
 
 public class ScreeningNotFoundException extends BusinessException {
 
-    private static final String SCREENING_NOT_FOUND_DESCRIPTION = "SCREENING_NOT_FOUND_DESCRIPTION";
-
-    private final String screeningId;
     public ScreeningNotFoundException(String screeningId) {
-        super(SCREENING_NOT_FOUND_DESCRIPTION);
-        this.screeningId = screeningId;
+        super(String.format("Screening not found: %s", screeningId));
     }
 }
